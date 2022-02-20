b2cf2a0c1fd003d41dbf8dfc02a88a674e66816f
Elchin Fahradov <elchin.fahradov@iakta.it>
2022-02-20 13:51:32 +0100

    fix: condition if qr exist added, and draw rectangle added

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -6,28 +6,32 @@ import {makeStyles} from '@mui/styles';
 
 function App() {
 
+  const [isLoaded, setloading] =  useState(false);
+
   const [scanResultWebCam, setScanResultWebCam] =  useState('');
   const [x1, setcoorx1] =  useState(0);
   const [y1, setcoory1] =  useState(0);
+
   const [x2, setcoorx2] =  useState(0);
   const [y2, setcoory2] =  useState(0);
+
   const [x3, setcoorx3] =  useState(0);
   const [y3, setcoory3] =  useState(0);
+
   const [x4, setcoorx4] =  useState(0);
   const [y4, setcoory4] =  useState(0);
   const classes = useStyles();
 
-
-
-
-let videoRef = useRef<HTMLVideoElement>(null);
-const canvasRef = useRef<HTMLCanvasElement>(null);
+  let videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
 
   useEffect(() => {
     getVideo();
   }, [videoRef]); 
 
+
+  ////-----------------------------Get video----------------
   const getVideo = () => {
     navigator.mediaDevices
       .getUserMedia({ video: { width: 300 }})
@@ -41,9 +45,11 @@ const canvasRef = useRef<HTMLCanvasElement>(null);
       .catch(err => {
         console.error("error:", err);
       });
-      setInterval(captureImage, 3000);
+      setInterval(captureImage, 1000);
   };
 
+
+///-----------------------------Capture Image-------------------
   var captureImage = function() {
     let video = videoRef.current;
     let canvas=canvasRef.current;
@@ -55,50 +61,66 @@ const canvasRef = useRef<HTMLCanvasElement>(null);
 
     ctx.drawImage(video, 0, 0, width, height);
 
-
     var newImg = document.createElement('img');
 
     newImg.src = canvasRef.current.toDataURL("png", 0.90);
 
     let base64Format= newImg.src;
 
+    
     const newBase64 = base64Format.substring(base64Format.indexOf('base64,') + 7);
+
+    //Call Function Rust
+    callRustFunc(newBase64);
+
+    
+  };
+
+  ////-----------------------------Draw rectangle----------------
+
+  var drawRectangle=()=>{
+
+    let canvas=canvasRef.current;
+    let ctx = canvas.getContext("2d");
 
     ctx.beginPath();
     ctx.lineWidth = 4;
     ctx.strokeStyle = "green";
 
-    ctx.rect(x1,x1,y1,y1);
-    ctx.arc(x1,x1, 10, 0, 2 * Math.PI, true);
+    ctx.arc(x1,y1, 10, 0, 2 * Math.PI, true);
+
+    ctx.lineTo(x1, y1); 
+    ctx.lineTo(x2, y2);
+
+    ctx.lineTo(x3, y3);
+    ctx.lineTo(x4, y4);
+
+    ctx.lineTo(x1, y1);
 
 
-    ctx.stroke();    
+    ctx.stroke();  
 
+    setloading(false);
 
-    callRustFunc(newBase64);
+  }
 
-  
-  };
+////----------------------------Call rust function--------------------
 
-  var callRustFunc= function(newBase64){
-
+  var callRustFunc= (newBase64: string)=>{
     import('wasm').then(({decode_qr}) => {
       const decodeQr = decode_qr(newBase64);
       
       setScanResultWebCam(decodeQr);
   
-    if(decodeQr!="[Error] No QR code detected in image"){ 
-  
-  
+    if(decodeQr!=="[Error] No QR code detected in image"){ 
+
       console.log(decodeQr);
   
       let a=decodeQr.split(",");
-  
+
       let X_BottomLeft=parseInt(a[0].replace(/[^0-9]/g, ""));
       let Y_BottomLeft=parseInt(a[1].replace(/[^0-9]/g, ""));
-  
-      console.log(X_BottomLeft, Y_BottomLeft, " asdf");
-  
+        
       let X_BottomRight=parseInt(a[2].replace(/[^0-9]/g, ""));
       let Y_BottomRight=parseInt(a[3].replace(/[^0-9]/g, ""));
   
@@ -107,6 +129,7 @@ const canvasRef = useRef<HTMLCanvasElement>(null);
   
       let X_TopLeft=parseInt(a[6].replace(/[^0-9]/g, ""));
       let Y_TopLeft=parseInt(a[7].replace(/[^0-9]/g, ""));
+
   
       setcoorx1(X_BottomLeft);
       setcoory1(Y_BottomLeft);
@@ -120,21 +143,19 @@ const canvasRef = useRef<HTMLCanvasElement>(null);
       setcoorx4(X_TopLeft);
       setcoory4(Y_TopLeft);
 
+      setloading(true);  
 
-
-
-      console.log(x1, y1, " x1,y1")
-  
-  
-      // console.log(X_BottomLeft+" "+Y_BottomLeft+" "+X_BottomRight+" "+Y_BottomRight+" "+X_TopRight+" "+Y_TopRight+" "+X_TopLeft+" "+Y_TopLeft);
-    }
+      }
     })
+
   }
 
 
 
   return ( 
 <div>
+  {console.log("render")}
+  {isLoaded && drawRectangle()}
   <Container className={classes.conatiner}>
      <Card>
      <h2 className={classes.title}>Scan QR Code</h2>
@@ -161,6 +182,10 @@ const canvasRef = useRef<HTMLCanvasElement>(null);
 
   );
 }
+
+
+
+
 const useStyles = makeStyles(() => ({
   conatiner: {
     marginTop: 10
